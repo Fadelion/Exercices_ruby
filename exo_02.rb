567911535f41db23a4b83f21f0371399426a8474
Fadelion <maigadede@gmail.com>
2025-04-16 01:27:03 +0000

    validation des push et modification des fichiers

--- exo_02.rb
+++ exo_02.rb
@@ -1,5 +1,5 @@
-#! /bin/local/ruby
-
+#!/usr/bin/ruby
+# encoding: utf-8
 # Programme pour affiche les lignes suivantes
 puts "Bonjour, monde !"
 puts "Et avec une voix sexy, ça donne: Bonjour, monde !"
